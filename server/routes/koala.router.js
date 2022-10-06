cbbf5ea831e50977dbf82b19a1da5c14f59e5554
Daniel Legan <legan.daniel@gmail.com>
2022-10-06 13:55:56 -0500

    finish koala.router pool query to db

--- server/routes/koala.router.js
+++ server/routes/koala.router.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const pool = require('../modules/pool');
 const koalaRouter = express.Router();
 
 // DB CONNECTION
@@ -11,6 +12,17 @@ const koalaRouter = express.Router();
 koalaRouter.post('/', (req, res) => {
     let newKoala = req.body;
     console.log('adding new koala', newKoala);
+
+    let queryText = `INSERT INTO "koalas" ("name", "gender", "age", "ready_to_transfer", "notes")
+                    VALUES ($1, $2, $3, $4, $5);`;
+    pool.query(queryText, [newKoala.name, newKoala.age, newKoala.gender, newKoala.readyForTransfer, newKoala.notes])
+        .then(result => {
+            res.sendStatus(201);
+        })
+        .catch(error => {
+            console.log(`error sending new koala`, error);
+            res.sendStatus(500);
+        });
 });
 
 // PUT
